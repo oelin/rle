93e92af2c1d4b9bed9af14ffc2ce58efdc7d1a22
oelin <42823429+oelin@users.noreply.github.com>
2023-01-24 18:54:18 +0000

    Create __init__.py

---
+++ rle/__init__.py
@@ -0,0 +1,23 @@
+def rle(string):
+    
+    encoded = ''
+    counter = 1
+    string += '\0'
+
+    for i, (a, b) in enumerate(zip(string[: -1], string[1 :])):
+
+        if a == b:
+            counter += 1
+
+        else:
+            code = f'{count}{a}'
+
+            if len(code) < counter:
+                encoding += code
+            
+            else:
+                encoding += a
+
+            counter = 1
+    
+    return encoding
